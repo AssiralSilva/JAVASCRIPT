e517eb1cc12f63b839d297a6fce4d5fccd3ee46b
AssiralSilva <larissa-jesus1016@outlook.com>
2023-10-17 20:00:31 -0300

    chore: teste1 - desafio calculadora

---
+++ Aulas/Parte 8/teste/script.js
@@ -0,0 +1,84 @@
+let n1 = document.querySelector(".n1")
+let n2 = document.querySelector(".n2")
+let n3 = document.querySelector(".n3")
+let n4 = document.querySelector(".n4")
+let n5 = document.querySelector(".n5")
+let n6 = document.querySelector(".n6")
+let n7 = document.querySelector(".n7")
+let n8 = document.querySelector(".n8")
+let n9 = document.querySelector(".n9")
+let visorResult = document.querySelector(".visor-box")
+
+function numero1() {
+    visorResult.value = '1';
+}
+
+n1.addEventListener("click", numero1)
+
+
+
+function numero2() {
+    visorResult.value = '2';
+}
+
+n2.addEventListener("click", numero2)
+
+
+
+function numero3() {
+    visorResult.value = '3';
+}
+
+n3.addEventListener("click", numero3)
+
+
+
+function numero4() {
+    visorResult.value = '4';
+}
+
+n4.addEventListener("click", numero4)
+
+
+
+function numero5() {
+    visorResult.value = '5';
+}
+
+n5.addEventListener("click", numero5)
+
+
+
+function numero6() {
+    visorResult.value = '6';
+}
+
+n6.addEventListener("click", numero6)
+
+
+
+function numero7() {
+    visorResult.value = '7';
+}
+
+n7.addEventListener("click", numero7)
+
+
+
+function numero8() {
+    visorResult.value = '8';
+}
+
+n8.addEventListener("click", numero8)
+
+
+
+function numero9() {
+    visorResult.value = '9';
+}
+
+n9.addEventListener("click", numero9)
+
+while (visorResult.value < 10) {
+    
+}
