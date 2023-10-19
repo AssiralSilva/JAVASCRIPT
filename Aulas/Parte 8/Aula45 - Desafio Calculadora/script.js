634561dc1cd702d4831574c05e16bc0198c16004
AssiralSilva <larissa-jesus1016@outlook.com>
2023-10-18 22:29:32 -0300

    chore: Ativando os botões de números

--- Aulas/Parte 8/Aula45 - Desafio Calculadora/script.js
+++ Aulas/Parte 8/Aula45 - Desafio Calculadora/script.js
@@ -7,78 +7,59 @@ let n6 = document.querySelector(".n6")
 let n7 = document.querySelector(".n7")
 let n8 = document.querySelector(".n8")
 let n9 = document.querySelector(".n9")
+let nzero = document.querySelector("#zero")
 let visorResult = document.querySelector(".visor-box")
+let visorValue = "";
 
-function numero1() {
-    visorResult.value = '1';
+let buttonReset = document.querySelector(".reset-button")
+
+function adicionarNumero(numero) {
+    visorValue += numero;
+    visorResult.value = visorValue;
 }
 
-n1.addEventListener("click", numero1)
+n1.addEventListener("click", () => {
+    adicionarNumero(1)
+})
+
+n2.addEventListener("click", () => {
+    adicionarNumero(2)
+})
+
+n3.addEventListener("click", () => {
+    adicionarNumero(3)
+})
+
+n4.addEventListener("click", () => {
+    adicionarNumero(4)
+})
+
+n5.addEventListener("click", () => {
+    adicionarNumero(5)
+})
+
+n6.addEventListener("click", () => {
+    adicionarNumero(6)
+})
+
+n7.addEventListener("click", () => {
+    adicionarNumero(7)
+})
+
+n8.addEventListener("click", () => {
+    adicionarNumero(8)
+})
+
+n9.addEventListener("click", () => {
+    adicionarNumero(9)
+})
 
 
-
-function numero2() {
-    visorResult.value = '2';
-}
-
-n2.addEventListener("click", numero2)
+nzero.addEventListener("click", () => {
+    adicionarNumero(0)
+})
 
 
-
-function numero3() {
-    visorResult.value = '3';
-}
-
-n3.addEventListener("click", numero3)
-
-
-
-function numero4() {
-    visorResult.value = '4';
-}
-
-n4.addEventListener("click", numero4)
-
-
-
-function numero5() {
-    visorResult.value = '5';
-}
-
-n5.addEventListener("click", numero5)
-
-
-
-function numero6() {
-    visorResult.value = '6';
-}
-
-n6.addEventListener("click", numero6)
-
-
-
-function numero7() {
-    visorResult.value = '7';
-}
-
-n7.addEventListener("click", numero7)
-
-
-
-function numero8() {
-    visorResult.value = '8';
-}
-
-n8.addEventListener("click", numero8)
-
-
-
-function numero9() {
-    visorResult.value = '9';
-}
-
-n9.addEventListener("click", numero9)
-
-while (visorResult.value < 10) {
-    
-}
+// buttonReset.addEventListener("click", () => {
+//     visorResult.value = "";
+// })
