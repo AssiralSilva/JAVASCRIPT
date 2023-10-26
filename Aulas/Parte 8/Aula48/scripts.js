3eefb4d5403c7c5b1c573ba96a8710ec68bcf488
AssiralSilva <larissa-jesus1016@outlook.com>
2023-10-26 13:36:03 -0300

    chore: HTML e CSS DO JokenPô

---
+++ Aulas/Parte 8/Aula48/scripts.js
@@ -0,0 +1,12 @@
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissors = document.querySelector(".scissors")
+
+
+
+
+function teste() {
+    if(onclick == rock) {
+        alert(rock)
+    }
+}
