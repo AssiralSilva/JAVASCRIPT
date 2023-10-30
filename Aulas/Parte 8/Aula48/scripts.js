d9d2550137c9244ff5b839ec79d99b10a499d10c
AssiralSilva <larissa-jesus1016@outlook.com>
2023-10-30 17:20:11 -0300

    chore: Me desafiei e consegui. Description: Me desafiei a construir a lógica do jogo JokenPô. E consegui.

--- Aulas/Parte 8/Aula48/scripts.js
+++ Aulas/Parte 8/Aula48/scripts.js
@@ -1,6 +1,13 @@
+const result = document.querySelector(".result")
 const rock = document.querySelector("#rock")
 const paper = document.querySelector("#paper")
 const scissors = document.querySelector("#scissors")
+const yourScore = document.querySelector(".your-score")
+const machineScore = document.querySelector(".machine-score")
+let machineChoice = 0
+
+let myScoreNumber = 0
+let machineScoreNumber = 0
 
 rock.addEventListener("click", () => {
     pontuaçao()
@@ -14,18 +21,19 @@ function pontuaçao(min = 1, max = 4) {
     machineChoice = Math.floor(Math.random() * (max - min) + min)
    
     if(machineChoice == 1)  {
-        const result = document.querySelector(".result")
         result.innerHTML = "Empate";
     }
 
     if(machineChoice == 2) {
-        const result = document.querySelector(".result")
         result.innerHTML = "Você Perdeu";
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
     }
 
     if(machineChoice == 3) {
-        const result = document.querySelector(".result")
         result.innerHTML = "Você Venceu";
+        myScoreNumber++
+        yourScore.innerHTML = myScoreNumber
     }
 }
 
@@ -36,18 +44,19 @@ paper.addEventListener("click", (min = 1, max = 2) => {
         machineChoice = Math.floor(Math.random() * (max - min) + min)
        
         if(machineChoice == 1)  {
-            const result = document.querySelector(".result")
             result.innerHTML = "Empate";
         }
 
         if(machineChoice == 2) {
-            const result = document.querySelector(".result")
             result.innerHTML = "Você Venceu";
+            myScoreNumber++
+            yourScore.innerHTML = myScoreNumber
         }
 
         if(machineChoice == 3) {
-            const result = document.querySelector(".result")
             result.innerHTML = "Você Perdeu";
+            machineScoreNumber++
+            machineScore.innerHTML = machineScoreNumber
         }
     }
 })
